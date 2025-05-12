4d334d040e3e7458ab9b2b34d9927e5f8e651ca8
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-05-12 15:16:13 +0200

    Add project references for Northwind.DataContext.Sqlite
    - Included project reference to Northwind.DataContext.Sqlite in both
      Northwind.Blazor and Northwind.Blazor.Client projects.
    - Added Using directive for Northwind.EntityModels in Program.cs.
    - Updated comments in Program.cs for clarity on context usage.

--- Northwind.Blazor.Client/Program.cs
+++ Northwind.Blazor.Client/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Northwind.EntityModels;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// We would need this path hint if we used an additional folder containing the blazor projects. see p. 717
+//builder.Services.AddNorthwindContext(relativePath: @"..\..");
+builder.Services.AddNorthwindContext();
 
 await builder.Build().RunAsync();
