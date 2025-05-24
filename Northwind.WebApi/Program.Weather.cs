8444061657b9b70c3734f1668e1b1061871b610b
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-05-24 21:33:35 +0200

    WebAPI: Refactor weather forecast logic and enhance documentation
    - Moved weather forecast management to Program.Weather.cs.
    - Added XML documentation for weather summaries and forecast methods.
    - Updated comments for clarity and consistency.

--- Northwind.WebApi/Program.Weather.cs
+++ Northwind.WebApi/Program.Weather.cs
@@ -1,12 +1,21 @@
 public partial class Program
 {
+    /// <summary>
+    /// Contains all possible weather summaries.
+    /// </summary>
     static string[] summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    /// <summary>
+    /// Get (pseudo) weather forecast for a given number of days
+    /// </summary>
+    /// <param name="days">Number of days to forecast</param>
+    /// <returns>new array of weather forecasts</returns>
     internal static WeatherForecast[]? GetWeather(int days)
     {
+        // Create all weather forecasts
         WeatherForecast[]? forecast = Enumerable.Range(1, days)
             .Select(index => new WeatherForecast
             (
@@ -19,9 +28,11 @@ public partial class Program
         return forecast;
     }
 
+    /// <summary>
+    /// Can be used to store data of a single weather forecast.
+    /// </summary>
     internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     {
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
-
 }
--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -9,10 +9,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); // Add openapi endpoint
 }
 
 app.UseHttpsRedirection();
+
+// Code to manage weather forecast moved to Program.Weather.cs
 
 app.MapGet("/weatherforecast/{days:int?}",
     (int? days) => GetWeather(days ?? 5))
