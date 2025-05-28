20f676606c47532795c233b4fe48a971177c68aa
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-05-28 09:19:24 +0200

    WebAPI: Update OpenAPI configuration in Program.cs
    - Changed OpenAPI registration to use document name "v2".
    - Added comment to separate service configuration section.

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -5,8 +5,9 @@ using Northwind.WebApi.Repositories; // for ICustomerRepository
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// =====================================================================
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(documentName: "v2");
 builder.Services.AddNorthwindContext();
 
 // Configure caching, see p. 750
