d02190e0c196d295e839398f80400dab577c439b
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-05-28 10:12:55 +0200

    WebAPI: Add logging configuration and HTTP logging middleware
    - Created example appsettings files for development and production.
    - Configured HTTP logging in Program.cs with request and response body limits.
    - Updated logging levels for ASP.NET Core components.

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Northwind.EntityModels; // for AddNorthwindContext method
 using Microsoft.Extensions.Caching.Hybrid; // for HybridCache
 using Northwind.WebApi.Repositories; // for ICustomerRepository
+using Microsoft.AspNetCore.HttpLogging; // for HttpLoggingFields
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,14 @@ builder.Services.AddHybridCache(options =>
 // Add CustomerRepository to the container
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
+// Add HTTP logging
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096; // Default is 32K
+    options.ResponseBodyLogLimit = 4096; // Default is 32K
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,6 +43,8 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v2.json", "Northwind API v2");
     });
 }
+
+app.UseHttpLogging();
 
 app.UseHttpsRedirection();
 
