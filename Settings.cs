7b3b08d774aa71738937732351995787614acaba
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-05-31 22:04:42 +0200

    Add Settings class and appsettings.json for configuration

---
+++ Settings.cs
@@ -0,0 +1,5 @@
+public sealed class Settings // class is in null namespace like Program class
+{
+    public required string ModelId { get; set; }
+    public required string OpenAISecretKey { get; set; }
+}
