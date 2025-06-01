e87c185bb7280974133106041506e799357677a0
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2025-06-01 11:40:03 +0200

    Implement main application logic for user interaction with Semantic Kernel

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel; // to use Kernel
+
+Settings? settings = GetSettings();
+
+if (settings is null)
+{
+    WriteLine("Settings not found or not valid");
+    return; // exit the app
+}
+
+Kernel kernel = GetKernel(settings);
+
+ConsoleKey key = ConsoleKey.A;
+
+while (key is not ConsoleKey.X)
+{
+    WriteLine("Enter your question: ");
+    string? question = ReadLine();
+
+    if (string.IsNullOrWhiteSpace(question))
+    {
+        WriteLine("No question provided. Please try again.");
+        continue;
+    }
+
+    WriteLine(await kernel.InvokePromptAsync(question));
+
+    WriteLine();
+
+    WriteLine("Press 'X' to exit or any other key to ask another question.");
+
+    key = ReadKey(intercept: true).Key;
+}
